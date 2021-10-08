77a5b3fc187a4b93d611a90dcc2474d1ea8f21c5
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-10-08 16:10:13 -0400

    Basic routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import { DMProvider} from "./utils/DMContext";
+import { DMProvider } from './utils/DMContext';
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import TestComponent from "./components/TestComponent";
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <DMProvider>
         <Header />
-        <TestComponent />
+        <Main />
       </DMProvider>
     </div>
   );
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,31 @@
-import React from "react";
+// ================================================
+//  Main is the Main Router that determines which
+// sub-page should be loaded.
+// ================================================
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-
-
-
-export default Main;
+export default function Main() {
+  return (
+    <Router>
+      {/* Navigation goes here */}
+      <Switch>
+        <Route path="/">
+          <p>Combat Tracker goes here</p>
+        </Route>
+        <Route path="/dice">
+          <p>Dice goes here</p>
+        </Route>
+        <Route path="/characters">
+          <p>Characters Goes here</p>
+        </Route>
+        <Route path="/abilities">
+          <p>abilities goes here</p>
+        </Route>
+        <Route path="/settings">
+          <p>settings goes here</p>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}

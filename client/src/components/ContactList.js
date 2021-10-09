c1164e99e12ef1a125b084d8de321667a05e1221
Nicholas-Herold <nherold86@gmail.com>
2021-10-09 13:46:08 -0400

    start of chat feature

---
+++ client/src/components/ContactList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ListItem,UnorderedList } from "@chakra-ui/react"
+
+function ContactList() {
+    return (
+    <UnorderedList>
+        <ListItem>Lorem ipsum dolor sit amet</ListItem>
+        <ListItem>Consectetur adipiscing elit</ListItem>
+        <ListItem>Integer molestie lorem at massa</ListItem>
+        <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+    </UnorderedList>
+    )
+}
+
+export default ContactList

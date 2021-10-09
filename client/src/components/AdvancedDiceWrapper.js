50a1bae9eae92953c3b6dcb52189910b33c5b52e
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-10-09 14:00:39 -0400

    Removed unnecessary text

--- client/src/components/AdvancedDiceWrapper.js
+++ client/src/components/AdvancedDiceWrapper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, Heading } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import AdvancedDiceInput from './AdvancedDiceInput';
 
 export const AdvancedDiceWrapper = ({ roller }) => {

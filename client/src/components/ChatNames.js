c85551206bec7e952e3e5c17cae694baf1d88b92
Nicholas-Herold <nherold86@gmail.com>
2021-10-12 00:07:38 -0400

    added some fetures inputs and styles to chat

---
+++ client/src/components/ChatNames.js
@@ -0,0 +1,15 @@
+
+import { Button } from '@chakra-ui/button'
+import { Container } from '@chakra-ui/layout'
+import React from 'react'
+
+
+function ChatNames() {
+    return (
+       <Container align = "stretch">
+           <Button value = "chatid"colorScheme="teal" variant="outline">billy</Button>
+       </Container>
+       
+    )
+}
+export default ChatNames

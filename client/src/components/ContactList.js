3c993199d3538adff7985c556e6319f4cf4fcec9
Nicholas-Herold <nherold86@gmail.com>
2021-10-14 01:47:52 -0400

    finally was able to retreive  user info from passport

--- client/src/components/ContactList.js
+++ client/src/components/ContactList.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input, } from "@chakra-ui/react"
 import ChatNames from './ChatNames'
+import axios from 'axios'
 
-function ContactList() {
+
+function ContactList(currentUser) {
+    const [chats,setChat] =useState([])
+    const user = currentUser
+
+    // useEffect(() => {
+    //     const getChats = async() =>{
+    //         const res = await axios.get('/')
+    //     }
+    //     return () => {
+    //         cleanup
+    //     }
+    // }, [input])
+
+   console.log(user)
     return (<>
         <Input placeholder="search friends" claseName = "findFriends"/>
         <ChatNames/>
         <ChatNames/>
         <ChatNames/>
+        
     </>
     )
 }

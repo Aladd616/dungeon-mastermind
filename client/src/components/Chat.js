da6821ecde62c2c1771d5a59fee9dc6c199d26ad
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-10-15 19:38:27 -0400

    Fixed Github's attempt to mangle chat

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,32 +1,32 @@
-// import React, { useEffect, useState } from 'react';
-// import Axios from 'axios';
-// import io from 'socket.io-client';
-// import {
-//   Drawer,
-//   DrawerFooter,
-//   DrawerOverlay,
-//   DrawerContent,
-//   DrawerCloseButton,
-//   useDisclosure,
-//   Button,
-//   Tabs,
-//   TabList,
-//   TabPanels,
-//   Tab,
-//   TabPanel,
-//   Textarea,
-//   FormControl,
-//   Box,
-// } from '@chakra-ui/react';
-// import ContactList from './ContactList';
-// import Messages from './messages';
+import React,{useState} from 'react'
+import Axios  from 'axios'
+import io from "socket.io-client"
+import {
+    Drawer,
+    DrawerFooter,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Button,
+    Tabs,
+    TabList,
+    TabPanels,
+    Tab,
+    TabPanel,
+    Textarea,
+    FormControl,
+    Box
+  } from "@chakra-ui/react"
+  import ContactList from './ContactList'
+import Messages from './messages'
 
 function Chat() {
     const { isOpen, onOpen, onClose } = useDisclosure()
 
 
-//   const [user, setUser] = useState('');
-//   const [newMessage, setNewMessage] = useState('');
+    const [user,setUser]=useState("")
+    const [newMessage,setNewMessage] = useState("")
 
 
 
@@ -41,14 +41,12 @@ function Chat() {
       });
     };
 
-//   function runfunctions() {
-//     onOpen();
-//     getUser();
-//   }
-//   //SOCKETio
+    function runfunctions(){
+        onOpen();
+        getUser();
 
-//   useEffect(() => {
-//     socketRef.current = io.connect('/');
+    }
+    //SOCKETio
 
     // socket.on("connect", () => {
     //   console.log(socket.id); // x8WIv7-mJelg7on_ALbx
@@ -90,70 +88,41 @@ function Chat() {
   </TabList>
 
 
-//   const submitMessage = (event) => {
-//     event.preventDefault();
-//     const messageObject = {
-//       body: message,
-//       id: yourID,
-//     };
-//     setMessage('');
-//     socketRef.current.emit('send message', messageObject);
-//   };
+  <TabPanels>
+    <TabPanel>
+      <ContactList currentUser = {user} />
+    </TabPanel>
+    <TabPanel>
+        <Box overflowY="auto"
+        height = "60vh"
+          >
+        <Messages/>
+        <Messages/>
+        <Messages/>
+        <Messages/>
+        <Messages/>
+        </Box>
+        <form onSubmit = {submitMessage}>
+        <FormControl>
+        <Textarea
+        onChange={(e) => setNewMessage(e.target.value)}
+        value = {newMessage}
+        />
+        </FormControl>
+        <Button type='submit'>Send</Button>
+        </form>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+            <DrawerFooter>
+              <Button variant="outline" mr={2} onClick={onClose}>
+               Close
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+}
 
-//   return (
-//     <>
-//       <Button ref={btnRef} colorScheme="teal" onClick={runfunctions}>
-//         Chat
-//       </Button>
-
-//       <Drawer
-//         isOpen={isOpen}
-//         placement="right"
-//         onClose={onClose}
-//         finalFocusRef={btnRef}
-//       >
-//         <DrawerOverlay />
-//         <DrawerContent>
-//           <DrawerCloseButton />
-//           <Tabs>
-//             <TabList>
-//               <Tab>Chats</Tab>
-//               <Tab>Messages</Tab>
-//             </TabList>
-
-//             <TabPanels>
-//               <TabPanel>
-//                 <ContactList currentUser={user} />
-//               </TabPanel>
-//               <TabPanel>
-//                 <Box overflowY="auto" height="60vh">
-//                   <Messages />
-//                   <Messages />
-//                   <Messages />
-//                   <Messages />
-//                   <Messages />
-//                 </Box>
-//                 <form onSubmit={submitMessage}>
-//                   <FormControl>
-//                     <Textarea
-//                       onChange={(e) => setNewMessage(e.target.value)}
-//                       value={newMessage}
-//                     />
-//                   </FormControl>
-//                   <Button type="submit">Send</Button>
-//                 </form>
-//               </TabPanel>
-//             </TabPanels>
-//           </Tabs>
-//           <DrawerFooter>
-//             <Button variant="outline" mr={2} onClick={onClose}>
-//               Close
-//             </Button>
-//           </DrawerFooter>
-//         </DrawerContent>
-//       </Drawer>
-//     </>
-//   );
-// }
-
-// export default Chat;
+export default Chat

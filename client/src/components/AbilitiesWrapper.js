aa2c7a794e26ef5e52a1def2fa5a223437c5470d
casukhelai <casukhelai@gmail.com>
2021-10-15 14:21:14 -0400

    created some color styling for abilities wrapper

--- client/src/components/AbilitiesWrapper.js
+++ client/src/components/AbilitiesWrapper.js
@@ -25,8 +25,8 @@ export const AbilitiesWrapper = (props) => {
   });
 
   return (
-    <Grid templateRows="repeat(1, 1f)" templateColumns="repeat(5, 1f)">
-      <GridItem colSpan={1}>
+    <Grid templateRows="repeat(1, 1f)" templateColumns="repeat(5, 1f)" gap={4} color='300C04'>
+      <GridItem colSpan={1} bg='#E5E1E0'>
         <Box>{renderList}</Box>
       </GridItem>
       <GridItem colSpan={4} colStart={2}>
